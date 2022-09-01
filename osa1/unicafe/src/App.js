9e14ea8c14a17c875d61280c71fa1265bc293560
Jere L <jere.leukkunen@outlook.com>
2022-09-01 18:12:29 +0300

    Add exercise 1.6

---
+++ osa1/unicafe/src/App.js
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <h1>statistics</h1>
+      <StatisticLine num={good} text="good" />
+      <StatisticLine num={neutral} text="neutral" />
+      <StatisticLine num={bad} text="bad" />
+    </div>
+  )
+}
+
+
+const StatisticLine = ({ text, num }) => (
+  <text>
+      {text} {num}<br/>
+  </text>
+)
+
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+export default App
