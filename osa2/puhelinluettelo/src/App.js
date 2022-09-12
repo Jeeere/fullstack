a0fe0266d622ad770a0dea018a1c19897aeb10f2
Jere L <jere.leukkunen@outlook.com>
2022-09-12 21:02:47 +0300

    Add exercise 2.6

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleInputChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    console.log("button clicked", event.target);
+    const copy = [...persons];
+    copy.push({ name: newName });
+    setPersons(copy);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleInputChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <h3 key={person.name}>{person.name}</h3>
+      ))}
+    </div>
+  );
+};
+
+export default App;
