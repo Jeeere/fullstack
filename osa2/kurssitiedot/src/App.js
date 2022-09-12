59082e9adb08c1d6d3ca32d59d9700f2d3ef8be8
Jere L <jere.leukkunen@outlook.com>
2022-09-12 20:26:54 +0300

    Add exercise 2.5

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,3 +1,5 @@
+import Course from "./modules/Course";
+
 const App = () => {
   const courses = [
     {
@@ -49,55 +51,6 @@ const App = () => {
       <h1>Web development curriculum</h1>
       <Course courses={courses} />
     </div>
-  );
-};
-
-const Course = ({ courses }) => {
-  console.log("Courses");
-  return (
-    <>
-      {courses.map((course) => (
-        <div key={course.id}>
-          <Header name={course.name} />
-          <Content parts={course.parts} />
-          <Total parts={course.parts} />
-        </div>
-      ))}
-    </>
-  );
-};
-
-const Header = ({ name }) => {
-  return <h2>{name}</h2>;
-};
-
-const Content = ({ parts, courseId }) => {
-  console.log("Content");
-  return (
-    <div>
-      {parts.map((part) => (
-        <Part name={part.name} exercises={part.exercises} key={part.id} />
-      ))}
-    </div>
-  );
-};
-
-const Part = ({ name, exercises }) => {
-  return (
-    <p>
-      {name} {exercises}
-    </p>
-  );
-};
-
-const Total = ({ parts }) => {
-  return (
-    <p>
-      <b>
-        Number of exercises{" "}
-        {parts.reduce((sum, part) => sum + part.exercises, 0)}
-      </b>
-    </p>
   );
 };
 
